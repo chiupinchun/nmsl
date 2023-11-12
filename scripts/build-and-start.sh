2880bc30c4eb38c688f1b60168584ec489f95a25
hato <fansintuesan@gmail.com>
2023-11-12 20:29:25 +0800

    修正pm2指令的參數

--- scripts/build-and-start.sh
+++ scripts/build-and-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/nmsl/main
 # yarn build
-pm2 list | grep "main" && pm2 restart main || pm2 start yarn --name "main" --interpreter bash -- start
+pm2 list | grep "main" && pm2 restart main || pm2 start yarn --name "main" -- start
